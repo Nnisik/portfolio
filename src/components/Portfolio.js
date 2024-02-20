6f48031b9319197240c8c2e8609c2b842d2314ba
Nnisik <nisik0120@gmail.com>
2024-02-20 18:56:54 +0300

    made a template for projects section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,15 @@
 function Project(props) {
     return (
-        <div className="project">
+        <div class="project">
+            <img class="progect-img" src={ props.img } alt="" />
+            <div class="project-descrip">
+                <h3>{ props.name }</h3>
+                <p>{ props.descrip }</p>
+                <div class="project-stack">
+                    <h3 class="tool">React JS</h3>
+                    <h3 class="tool">PHP</h3>
+                </div>
+            </div>
         </div>
     );
 }
@@ -8,6 +17,8 @@ function Project(props) {
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
+            <Project />
+            <Project />
             <Project />
         </div>
     );
